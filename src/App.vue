b1521b179ba8ea267639ca2a2af5d1c5b6ca345b
Joe B <baumerj@gmail.com>
2021-03-31 20:42:30 -0400

    feat: landing page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-<div>
-  Landing
-</div>
+  <div>
+    <img src="./assets/aftr-market-landing.png" />
+  </div>
 </template>
 
 <script>
 
 </script>
+
+<style scoped>
+div {
+  width: 100vw;
+  height: 100vh;
+  background: #000;
+}
+
+img {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 40%;
+  min-width: 200px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,8 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
-//import { uuid } from 'vue-uuid'; 
 import './index.css';
 
 import App from './App.vue';
-import VehicleList from './components/VehicleList.vue';
-import CreateVehicle from './components/CreateVehicle.vue';
-import VehicleInfo from './components/VehicleInfo.vue';
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        //{ path: '/', redirect: '/vehicles'},
-        { name: 'vehicles', path: '/vehicles', component: VehicleList, alias: '/', },
-        { name: 'vehicle-info', path: '/vehicles/:vehicleId', component: VehicleInfo, props: true },
-        { name: 'create', path: '/create', component: CreateVehicle },
-        { path: '/:notFound(.*)', redirect: '/' }
-    ],
-    linkActiveClass: 'active'
-});
 
 const app = createApp(App);
-app.use(router);
-//app.use(uuid);
-
 app.mount('#app');
