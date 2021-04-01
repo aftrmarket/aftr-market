7121adf6de88954bc451dece78b8a4644da65ebe
Joe B <baumerj@gmail.com>
2021-03-31 20:23:00 -0400

    feat: landing

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
 <div>
-  <the-header></the-header>
-  <router-view></router-view>
+Landing
 </div>
 </template>
 
 <script>
-
-import TheHeader from './components/layouts/TheHeader.vue';
-
-export default {
-  components: { TheHeader }
-}
 
 </script>
