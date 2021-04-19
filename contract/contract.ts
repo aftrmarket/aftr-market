9e15d1da6c885500a5d894f98fdb9be5e1242a68
Joe B <baumerj@gmail.com>
2021-04-18 20:44:07 -0400

    feat: Added contract template

---
+++ contract/contract.ts
@@ -0,0 +1,11 @@
+import { StateInterface, ActionInterface, BalanceInterface, InputInterface } from "./faces";
+
+export function handle(state: StateInterface, action: ActionInterface) {
+  const balances = state.balances;
+  const input = action.input;
+  const caller = action.caller;
+
+  if (input.function === "transfer") {
+
+  }
+}
---
+++ contract/faces.ts
@@ -0,0 +1,24 @@
+export interface StateInterface {
+  ticker: string;
+  reserve: string;
+  balances: {
+    [addr: string]: number;
+  };
+}
+
+export interface BalanceInterface {
+  target: string;
+  balance: number;
+  ticker: string;
+}
+
+export interface ActionInterface {
+  input: InputInterface;
+  caller: string;
+}
+
+export interface InputInterface {
+  function: "transfer" | "balance";
+  target?: string;
+  qty?: number;
+}
