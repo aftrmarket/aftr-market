6a0bae8c892691bfd1080ff7491b62450f6e5b50
Joe B <baumerj@gmail.com>
2021-08-13 16:50:26 -0400

    feat: vehicle editing scafold

--- src/components/vehicle/VehicleInfo.vue
+++ src/components/vehicle/VehicleInfo.vue
@@ -13,170 +13,163 @@
                 <p class="text-gray-900">Current Value: <span class="px-2 py-3 sm:px-6 inline-flex leading-5 font-semibold rounded-full bg-green-100 text-green-800">29,308,420.3323 AR</span></p>
             </div>
         </div>
-        <div class="px-4 sm:px-6 max-w-2xl text-sm text-gray-500">Performance</div>
-        <div class="flex items-center justify-between pb-4">
-            <div class="px-4 sm:px-6 text-semibold">1M: <span :class="vehicles[0].perf1m < 0 ? 'text-aftrRed' : 'text-green-600'">{{ vehicles[0].perf1m }}</span></div>
-            <div class="px-4 sm:px-6 text-semibold">3M: <span :class="vehicles[0].perf3m < 0 ? 'text-aftrRed' : 'text-green-600'">{{ vehicles[0].perf3m }}</span></div>
-            <div class="px-4 sm:px-6 text-semibold">Max: <span :class="vehicles[0].perfMax < 0 ? 'text-aftrRed' : 'text-green-600'">{{ vehicles[0].perfMax }}</span></div>
+        <div class="flex items-start justify-between">
+            <div>
+                <div class="px-4 sm:px-6 max-w-2xl text-sm text-gray-500">Status</div>
+                <div v-if="!allowVehicleEdits" class="flex items-center justify-between pb-4">
+                    <div class="px-4 sm:px-6"><span :class="vehicleStatusAlert">{{ vehicleStatusText }}</span></div>
+                </div>
+                <div v-else class="px-4 sm:px-6">
+                    <SwitchGroup as="div" class="flex items-center">
+                        <Switch v-model="statusSwitchEnabled" @click="statusChange" :class="[statusSwitchEnabled ? 'bg-green-600' : 'bg-gray-200', 'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500']">
+                            <span aria-hidden="true" :class="[statusSwitchEnabled ? 'translate-x-5' : 'translate-x-0', 'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200']" />
+                        </Switch>
+                        <SwitchLabel as="span" class="ml-3">
+                            <span :class="vehicleStatusAlert">{{ vehicleStatusText }}</span>
+                        </SwitchLabel>
+                    </SwitchGroup>
+                </div>
+            </div>
+            <div class="flex items-start justify-between">
+                <div>
+                    <div class="max-w-2xl text-sm text-gray-500">Editing</div>
+                    <div class="text-sm">
+                        <div class="flex items-center">
+                            <svg v-if="getActiveAddress === creatorAddress" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#065F46">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#991B1B">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                            </svg>
+                            Creator
+                        </div>
+                        <div class="flex items-center">
+                            <svg v-if="vehicle.status === 'stopped' || typeof vehicle.status === 'undefined'" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#065F46">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#991B1B">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                            </svg>
+                            Status = Not Running
+                        </div>
+                        <div class="flex items-center">
+                            <svg v-if="vehicle.ownership === 'single'" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#065F46">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="#991B1B">
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+                            </svg>
+                            Single Ownership
+                        </div>
+                        <div :class="editText">{{ editMessage }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { Switch, SwitchGroup, SwitchLabel } from '@headlessui/vue'
+import { mapGetters } from 'vuex';
+
+
 export default {
-    props: ["vehicle", "contractId"],
+    props: ['vehicle', 'contractId'],
+    components: { Switch, SwitchGroup, SwitchLabel },
     data() {
         return {
-            vehicles: [
-                {
-                    id: "6adfd3a1-cb33-4970-8e88-c2d0defa66ec",
-                    name: "Investment Bus",
-                    desc: "",
-                    profile: "",
-                    leasedSeats: 143,
-                    perf1m: -2.43,
-                    perf3m: 20.4,
-                    perfMax: 140.29,
-                    rating: "A+",
-                    psts: [
-                        {
-                            id: "46c0bdd1-56a9-4179-8a56-164b702a5cb8",
-                            ticker: "AFTR",
-                            name: "AFTR Market",
-                            price: 0.05,
-                            tokens: 1000,
-                            total: 50,
-                        },
-                        {
-                            id: "f9fde4f9-89ec-404b-8a0a-1bc674b39676",
-                            ticker: "XYZ",
-                            name: "CommunityXYZ",
-                            price: 0.098,
-                            tokens: 2000,
-                            total: 196,
-                        },
-                        {
-                            id: "e6125855-8414-4ae1-b611-66aff71160a2",
-                            ticker: "VRT",
-                            name: "Verto Exchange",
-                            price: 0.1,
-                            tokens: 2500,
-                            total: 250,
-                        },
-                    ],
-                    lessees: [
-                        {
-                            id: "b1898f1f-e1cd-4cdc-8797-56e4b91b7ed4",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "74834e69-a83b-4f82-8a13-17541061d02a",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "b026557f-6ecc-4a88-9eb6-3312772e64fe",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "29105359-d468-4ddb-bb08-cf13084c5c0f",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "53845c0e-73e2-424f-b76d-f32f53671149",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "12fc5148-827b-469f-88bf-1436f41a8d6f",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "051689e0-f6be-4037-b686-1375d3cb0cfe",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "797854a9-ba19-4c2e-bb35-62e0fd84ec7e",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "750dd121-e364-4b0c-933d-c787803bde62",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "3ecb9428-d7cd-41cb-bce0-0f31935d0000",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "eff9d33e-4f26-44d6-ba70-0059abf6516e",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "55becbe8-1ee9-4304-bd4a-f435bf8a6513",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "00b653b7-cfe1-410c-bdca-43726cfbe3a6",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "dbfe040d-bfcc-4879-ae0e-e6bde059a7f0",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                        {
-                            id: "7422d01b-8287-4703-8e03-0662913cafbc",
-                            leasedDate: "2021-03-28",
-                            seats: 3,
-                            pstsEarned: 14,
-                            earnings: 10022.32,
-                        },
-                    ],
-                },
-            ]
+            allowVehicleEdits: false,
         };
     },
+    watch: {
+        arConnected(value) {
+            // Allow edits if user is creator and vehicle is not running, otherwise changes must be via vote
+            if (value && this.getActiveAddress === this.creatorAddress && this.vehicle.status !== 'started') {
+                this.allowVehicleEdits = true;
+            } else {
+                this.allowVehicleEdits = false;
+            }
+        },
+        statusSwitchEnabled() {
+            // If vehicle is turned on, a vote must take place to turn it off (unless it's a single-owned vehicle)
+            if (this.getActiveAddress === this.creatorAddress && (this.vehicle.status !== 'started' || this.vehicle.ownership === 'single')) {
+                this.allowVehicleEdits = true;
+            } else {
+                this.allowVehicleEdits = false;
+            }
+        }
+    },
+    computed: {
+        vehicleStatusAlert() {
+            if (typeof this.vehicle.status === 'undefined' || this.vehicle.status === 'stopped' || this.vehicle.status === '') {
+                return "capitalize px-2 sm:px-4 py-3 inline-flex leading-3 text-sm font-semibold rounded-full bg-red-100 text-red-800";
+            } else if (this.vehicle.status === 'started') {
+                return "capitalize px-2 sm:px-4 py-3 inline-flex leading-3 text-sm font-semibold rounded-full bg-green-100 text-green-800";
+            } else {
+                return "capitalize px-2 sm:px-4 py-3 inline-flex leading-3 text-sm font-semibold rounded-full bg-yellow-100 text-yellow-800";
+            }
+        },
+        vehicleStatusText() {
+            if (typeof this.vehicle.status === 'undefined' || this.vehicle.status === '') {
+                return 'not running';
+            } else {
+                return this.vehicle.status;
+            }
+        },
+        editMessage() {
+            if (this.allowVehicleEdits) {
+                return "Edits allowed";
+            } else {
+                return "Votes must be passed to edit";
+            }
+        },
+        editText() {
+            if (this.allowVehicleEdits) {
+                return "p-2 text-green-800";
+            } else {
+                return "p-2 text-red-800";
+            }
+        },
+        creatorAddress() {
+            if (typeof this.vehicle.creator === 'undefined' || this.vehicle.creator === null || this.vehicle.creator === '') {
+                return '~NO ONE~';
+            } else {
+                return this.vehicle.creator;
+            }
+        },
+        ...mapGetters(['arConnected', 'getActiveAddress']),
+    },
     methods: {
+        statusChange() {
+            if (this.statusSwitchEnabled) {
+                this.vehicle.status = 'started';
+            } else {
+                this.vehicle.status = 'stopped';
+            }
+            /***** Update the contract *****/
+            /***** TODO - Will be ASYNC *****/
+        },
+        editingCheckbox(bool) {
+            if (bool) {
+                return '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" /></svg>';
+            } else {
+                return '<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" /></svg>';
+            }
+        },
+    },
+    created() {
+        if (this.getActiveAddress === this.creatorAddress && (this.vehicle.status !== 'started' || this.vehicle.ownership === 'single')) {
+            this.allowVehicleEdits = true;
+        } else {
+            this.allowVehicleEdits = false;
+        }
+    },
+    setup() {
+        const statusSwitchEnabled = ref(false)
 
+        return { statusSwitchEnabled, }
     },
 };
 </script>
