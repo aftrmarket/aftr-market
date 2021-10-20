b9ccb8d1defd0d9245590708e484b223e8da98c2
Joe B <baumerj@gmail.com>
2021-10-20 11:59:29 -0400

    chore: formatting

--- src/components/vehicle/VehicleMembers.vue
+++ src/components/vehicle/VehicleMembers.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="pt-4 w-full grid grid-cols-3 gap-4">
+    <div :class="editClass">
         <vehicle-members-add v-if="showAddModal" :vehicle="vehicle" @close="closeModal"></vehicle-members-add>
         <!-- Member Table -->
         <div class="col-span-3 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table class="min-w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                     <tr>
-                        <th scope="col" class="px-2 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th v-if="allowEdits" scope="col" class="px-2 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">
                             <input type="checkbox" v-model="selectAll" :class="checkboxClass" />
                         </th>
                         <th scope="col" class="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Member ({{ Object.keys(vehicle.balances).length }})
-                            <button @click.prevent="addMemberRow" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
+                            <button v-if="allowEdits" @click.prevent="addMemberRow" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                     <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
                                 </svg>
@@ -20,7 +20,7 @@
                         <th scope="col" class="px-4 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
                             Tokens
                         </th>
-                        <th scope="col" class="px-4 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        <th v-if="allowEdits" scope="col" class="px-4 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
                             New Allocation
                         </th>
                         <th v-show="addRow">
@@ -30,7 +30,7 @@
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
                     <tr v-for="(tokens, addr) in vehicle.balances" :key="addr" class="text-xs text-gray-500 hover:bg-gray-50">
-                        <td class="text-center px-2 py-2">
+                        <td v-if="allowEdits" class="text-center px-2 py-2">
                             <input type="checkbox" :value="addr" v-model="memberRemoves" :class="checkboxClass" />
                         </td>
                         <td class="text-left px-4 py-2">
@@ -39,7 +39,7 @@
                         <td class="text-right px-4 py-2">
                             {{ formatNumber(tokens) }}
                         </td>
-                        <td class="text-right px-4 py-2">
+                        <td v-if="allowEdits" class="text-right px-4 py-2">
                             <input type="number" v-model="memberUpdates[addr]" @blur="onDirty" class="mt-1 mb-1 mr-4 w-36 text-xs text-right focus:ring-aftrBlue focus:border-aftrBlue shadow-sm border-gray-300 rounded-md" />
                         </td>
                         <td v-show="addRow" class="text-right px-4 py-2">
@@ -113,10 +113,10 @@
                                 {{ index + 1 }}
                             </td>
                             <td class="px-4 py-2">
-                                {{ proposedText(key, memberUpdates[key], 'update') }}
+                                <span v-html="proposedText(key, memberUpdates[key], 'update')"></span>
                             </td>
                             <td class="px-4 py-2 text-center">
-                                <button @click.prevent="removeProposal(key, 'update')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
+                                <button @click.prevent="removeProposal(key, 'update')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrRed bg-white hover:bg-aftrRed hover:text-white focus:outline-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
                                     </svg>
@@ -146,10 +146,10 @@
                                 {{ index + 1 }}
                             </td>
                             <td class="px-4 py-2">
-                                {{ proposedText(member.recipient, member.qty, 'add') }}
+                                <span v-html="proposedText(member.recipient, member.qty, 'add')"></span>
                             </td>
                             <td class="px-4 py-2 text-center">
-                                <button @click.prevent="removeProposal(member.recipient, 'add')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
+                                <button @click.prevent="removeProposal(member.recipient, 'add')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrRed bg-white hover:bg-aftrRed hover:text-white focus:outline-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
                                     </svg>
@@ -179,10 +179,10 @@
                                 {{ index + 1 }}
                             </td>
                             <td class="px-4 py-2">
-                                {{ proposedText(member, vehicle.balances[member], 'remove') }}
+                                <span v-html="proposedText(member, vehicle.balances[member], 'remove')"></span>
                             </td>
                             <td class="px-4 py-2 text-center">
-                                <button @click.prevent="removeProposal(member, 'remove')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
+                                <button @click.prevent="removeProposal(member, 'remove')" type="button" class="inline-flex items-center px-1 py-1 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrRed bg-white hover:bg-aftrRed hover:text-white focus:outline-none">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 000 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
                                     </svg>
@@ -197,6 +197,7 @@
         <!--- Update Information End --->
 
         <div class="col-span-3 flex flex-col inline-flex">
+            <!---
             <div v-if="false && allowVehicleAdds && !arConnected" class="pt-6 flex justify-start items-center">
                 <button @click="arConnect" type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-aftrBlue hover:bg-aftrBlue-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                     Login to ArConnect
@@ -205,6 +206,7 @@
                     You must provide a wallet in order to add members to a vehicle
                 </label>
             </div>
+            
             <div v-if="allowEdits">
                 <button @click.prevent="addMemberModal" type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -213,7 +215,8 @@
                     <span class="pl-2">Add Member</span>
                 </button>
             </div>
-            <div v-if="numChanges > 0">
+            --->
+            <div v-if="numChanges > 0" class="text-right">
                 <button @click.prevent="submit" type="button" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
@@ -224,6 +227,7 @@
             </div>
         </div>
     </div>
+    ALLOW EDITS: {{ allowEdits }}
 </template>
 
 <script>
@@ -243,8 +247,8 @@ export default {
             newMember: '',
             newQty: null,
             addRow: false,
-            allowVehicleAdds: false,            // Allow adding members if Creator and Not Started
-            allowVehicleRemoves: false,         // Allow removing members if Creator, Single Ownership, and Not Started
+            // allowVehicleAdds: false,            // Allow adding members if Creator and Not Started
+            // allowVehicleRemoves: false,         // Allow removing members if Creator, Single Ownership, and Not Started
             showAddModal: false,                 // ADD THIS FUNCTIONALITY LATER
             allowEdits: false,
             isDirty: false,
@@ -252,7 +256,8 @@ export default {
     },
     watch: {
         arConnected() {
-            this.setFlags();
+            //this.setFlags();
+            this.checkEditStatus();
         },
     },
     computed: {
@@ -268,6 +273,13 @@ export default {
                     }
                 }
                 this.memberRemoves = selected;
+            }
+        },
+        editClass() {
+            if (this.allowEdits) {
+                return 'pt-4 w-full grid grid-cols-3 gap-4';
+            } else {
+                return 'pt-4 w-3/4 grid grid-cols-3 gap-4';
             }
         },
         numChanges() {
@@ -298,19 +310,18 @@ export default {
         },
         proposedText(recipient, qty, type) {
             if (type === 'update') {
-                //"Burn " + this.formatNumber(String(currentQty - qty)) + " for " + recipient;
                 const currentQty = this.vehicle.balances[recipient];
                 if (currentQty > +qty) {
-                    return "Burn " + this.formatNumber(String(currentQty - +qty)) + " for " + recipient;
+                    return "<span style='color:#FF6C8C'><b>Burn</b></span> " + this.formatNumber(String(currentQty - +qty)) + " tokens for <b> " + recipient + "</b>";
                 } else if (currentQty < +qty) {
-                    return "Mint " + this.formatNumber(String(+qty - currentQty)) + " for " + recipient;
+                    return "<span style='color:green'><b>Mint</b></span> " + this.formatNumber(String(+qty - currentQty)) + " tokens for <b>" + recipient + "</b>";
                 } else if (currentQty === +qty) {
                     return "No change for " + recipient;
                 }
             } else if (type === 'add') {
-                return "Add " + recipient + ", minting " + qty + " tokens";
+                return "<span style='color:green'><b>Add</b></span> <b>" + recipient + "</b>, minting " + qty + " tokens";
             } else if (type === 'remove') {
-                return "Remove " + recipient + ", burning " + qty + " tokens";
+                return "<span style='color:#FF6C8C'><b>Remove</b></span> <b>" + recipient + "</b>, burning " + qty + " tokens";
             }
         },
         removeProposal(member, type) {
@@ -322,16 +333,16 @@ export default {
                 this.memberRemoves = this.memberRemoves.filter((el) => el !== member);
             }
         },
-        setFlags() {
-            // Allow member add/remove if user is creator and (ownership is single or vehicle is not running), otherwise changes must be via vote
-            if (this.getActiveAddress === this.creatorAddress && (this.vehicle.ownership === 'single' || this.vehicle.status !== 'started')) {
-                this.allowVehicleAdds = true;
-                this.allowVehicleRemoves = true;
-            } else {
-                this.allowVehicleAdds = false;
-                this.allowVehicleRemoves = false;
-            }
-        },
+        // setFlags() {
+        //     // Allow member add/remove if user is creator and (ownership is single or vehicle is not running), otherwise changes must be via vote
+        //     if (this.getActiveAddress === this.creatorAddress && (this.vehicle.ownership === 'single' || this.vehicle.status !== 'started')) {
+        //         this.allowVehicleAdds = true;
+        //         this.allowVehicleRemoves = true;
+        //     } else {
+        //         this.allowVehicleAdds = false;
+        //         this.allowVehicleRemoves = false;
+        //     }
+        // },
         checkEditStatus() {
             // If wallet is in balances, then user can edit
             if (this.getActiveAddress in this.vehicle.balances) {
