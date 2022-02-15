06daf057e9843c86b3f363cd6207233bf2ff14b9
arcj0 <baumerj@gmail.com>
2022-02-15 11:52:30 -0500

    chore: footer cleanup

--- src/components/layouts/TheFooter.vue
+++ src/components/layouts/TheFooter.vue
@@ -8,7 +8,6 @@
     </div>
     <div v-else>
         <div class="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-            <div class="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left"></div>
             <div class="flex justify-end px-4 text-black font-sans text-lg gap-x-4 gap-y-0">
                 <div>Join our community</div>
                 <a href="https://twitter.com/AftrMarket" target="_blank"><img src="../../assets/Twitter-Logo-Blue.svg" width="40"></a>
