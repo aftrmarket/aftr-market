cb30aa0aea6297b2f31d8c6c11c07186b1d4fd40
arcj0 <baumerj@gmail.com>
2022-02-17 12:15:37 -0500

    chore: changed link color

--- src/components/vehicle/VehicleInfo.vue
+++ src/components/vehicle/VehicleInfo.vue
@@ -40,7 +40,7 @@
                     <label for="newLogo" class="py-2 block text-sm font-medium text-gray-700">Logo</label>
                     <div class="mt-2 sm:mt-0 sm:col-span-2 pl-6">
                         <div class="flex text-sm text-gray-600">
-                            <label for="newLogo" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                            <label for="newLogo" class="relative cursor-pointer bg-white rounded-md font-medium text-aftrBlue hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                 <span>Change the Logo</span>
                                 <input @change="onFileChange" id="newLogo" name="newLogo" type="file" class="sr-only" />
                             </label>
