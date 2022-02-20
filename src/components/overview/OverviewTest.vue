16d82895e8dd51c978ec18cd2657e09dee5c9ce5
arcj0 <baumerj@gmail.com>
2022-02-20 12:21:29 -0500

    feat: finished overvew test page

--- src/components/overview/OverviewTest.vue
+++ src/components/overview/OverviewTest.vue
@@ -119,81 +119,87 @@
             </div>
         </div>
 
+        <div class="relative bg-gray-50 pt-16 pb-32 overflow-hidden">
+            <div class="relative max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
+                <h3 class="pb-4 text-2xl font-extrabold text-gray-900 text-left tracking-tight sm:text-3xl">What Happens When I Press the Launch Button?</h3>
+                <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                    <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+                        <div>
+                            <div>
+                                <span class="h-12 w-12 rounded-md flex items-center justify-center bg-aftrBlue">
+                                    <PencilAltIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                                </span>
+                            </div>
+                            <div class="mt-6">
+                                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Explore an AFTR Vehicle</h2>
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-1.svg" /> Click on the Blue Horizon vehicle</p>
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Make sure you connect your wallet with ArConnect.</li>
+                                </ul>
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-2.svg" /> Enable Editing</p>
 
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Editing is available if you're a member of the vehicle</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Single Owner vs. DAO Owned</li>
+                                    <li class="text-sm text-gray-900 pl-16">Changes to a Single Owner vehicle occur during the next block after the change. Changes to a DAO Owned vehicle propose a vote for the members of the vehicle to vote on.</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Make a change and notice that a vote is proposed</li>
+                                </ul>
 
-  <div class="relative bg-gray-50 pt-16 pb-32 overflow-hidden">
-    <div class="relative">
-      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-          <div>
-            <div>
-              <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                <InboxIcon class="h-6 w-6 text-white" aria-hidden="true" />
-              </span>
-            </div>
-            <div class="mt-6">
-              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Explore an AFTR Vehicle</h2>
-              <p class="mt-4 text-lg text-gray-500">
-                  - Click on the Blue Horizon vehicle<br/>
-                  - Enable Editing (Editing is available if you're a member of the vehicle)<br/>
-                  - Add Some Tokens<br/>
-              </p>
-              <div class="mt-6">
-                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
-              </div>
-            </div>
-          </div>
-          <div class="mt-8 border-t border-gray-200 pt-6">
-            <blockquote>
-              <div>
-                <p class="text-base text-gray-500">&ldquo;Cras velit quis eros eget rhoncus lacus ultrices sed diam. Sit orci risus aenean curabitur donec aliquet. Mi venenatis in euismod ut.&rdquo;</p>
-              </div>
-              <footer class="mt-3">
-                <div class="flex items-center space-x-3">
-                  <div class="flex-shrink-0">
-                    <img class="h-6 w-6 rounded-full" src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" />
-                  </div>
-                  <div class="text-base font-medium text-gray-700">Marcia Hill, Digital Marketing Manager</div>
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-3.svg" /> Add Some Tokens</p>
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Transfer assets from your wallet to the vehicle</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Only assets that support cross contract communication can be added</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="mt-12 sm:mt-16 lg:mt-0">
+                        <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                            <img class="w-full lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="../../assets/overview-test-explore.svg" alt="" />
+                        </div>
+                    </div>
                 </div>
-              </footer>
-            </blockquote>
-          </div>
-        </div>
-        <div class="mt-12 sm:mt-16 lg:mt-0">
-          <div class="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg" alt="Inbox user interface" />
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="mt-24">
-      <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
-          <div>
-            <div>
-              <span class="h-12 w-12 rounded-md flex items-center justify-center bg-indigo-600">
-                <SparklesIcon class="h-6 w-6 text-white" aria-hidden="true" />
-              </span>
             </div>
-            <div class="mt-6">
-              <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Better understand your customers</h2>
-              <p class="mt-4 text-lg text-gray-500">Semper curabitur ullamcorper posuere nunc sed. Ornare iaculis bibendum malesuada faucibus lacinia porttitor. Pulvinar laoreet sagittis viverra duis. In venenatis sem arcu pretium pharetra at. Lectus viverra dui tellus ornare pharetra.</p>
-              <div class="mt-6">
-                <a href="#" class="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700"> Get started </a>
-              </div>
+            <div class="mt-24">
+                <div class="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+                    <div class="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+                        <div>
+                            <div>
+                                <span class="h-12 w-12 rounded-md flex items-center justify-center bg-aftrBlue">
+                                    <TruckIcon class="h-6 w-6 text-white" aria-hidden="true" />
+                                </span>
+                            </div>
+                            <div class="mt-6">
+                                <h2 class="text-3xl font-extrabold tracking-tight text-gray-900">Create Your Own Vehicle</h2>
+                                
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-1.svg" /> Make it a Single Owner Vehicle</p>
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Make sure you connect your wallet with ArConnect.</li>
+                                </ul>
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-2.svg" /> Add Some Tokens</p>
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Transfer assets from your wallet to the vehicle</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Only assets that support cross contract communication can be added</li>
+                                </ul>
+                                <p class="mt-4 flex flex-nowrap gap-2 text-lg text-gray-500"><img class="w-6" src="../../assets/overview-bullet-3.svg" /> Make a Change and Watch it Change on the Next Block</p>
+                                <ul>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Editing is available if you're a member of the vehicle</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Single Owner vs. DAO Owned</li>
+                                    <li class="text-sm text-gray-900 pl-16">Changes to a Single Owner vehicle occur during the next block after the change. Changes to a DAO Owned vehicle propose a vote for the members of the vehicle to vote on.</li>
+                                    <li class="text-base text-gray-500 pl-8 list-disc list-inside">Make a change and notice that a vote is proposed</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+                        <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
+                            <img class="w-full lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="../../assets/overview-test-create.svg" alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-        <div class="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
-          <div class="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-            <img class="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none" src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg" alt="Customer profile user interface" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
 
     </main>
 </template>
@@ -204,8 +210,7 @@ import {
     ClipboardCheckIcon,
     CashIcon,
     TruckIcon,
-    InboxIcon,
-    SparklesIcon,
+    PencilAltIcon,
 } from "@heroicons/vue/outline";
 
 import vertoInitState from "./../../testnet/contracts/vertoInitState.json";
@@ -250,7 +255,7 @@ const initProcess = [
 ];
 
 export default {
-    components: {     InboxIcon, SparklesIcon },
+    components: { PencilAltIcon, TruckIcon },
     data() {
         return {
             /** Smartweave variables */
