8b17b7bcaf7c4fce7395ea8becb05c7fda2ab6ab
arcj0 <baumerj@gmail.com>
2022-04-03 20:27:40 -0400

    chore: updated colors

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-black flex h-screen justify-center items-center">
     <div class="text-center space-y-20">
-      <img class="" src="./assets/aftr-market-landing.png" />
+      <img class="" src="./assets/aftr-market-landing1.png" />
       <p class="text-white text-4xl">Get Ready To... Come Along For The Ride</p>
       <h3 class="text-aftrDarkGrey-light text-2xl animate-pulse">Launching Soon in the <a class="underline" href="https://arweave.org" target="_blank">Arweave</a> ecosystem!</h3>
     </div>
