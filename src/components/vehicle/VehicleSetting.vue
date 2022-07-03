be389cfbe28a533e7a16475c99852ccaa484ef44
arcj0 <baumerj@gmail.com>
2022-07-03 16:19:49 -0400

    chore: changed add icon

--- src/components/vehicle/VehicleSetting.vue
+++ src/components/vehicle/VehicleSetting.vue
@@ -22,7 +22,7 @@
                             <span class="py-2">Key ({{ Object.keys(vehicle.settings).length }})</span>
                             <button v-if="uiEditMode" @click.prevent="addSettingRow" type="button" class="px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                                    <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
+                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
                                 </svg>
                             </button>
                         </th>
