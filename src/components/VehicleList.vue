a4e409b6eb8b0e8160ed9d8c89743f5abe159861
arcj0 <baumerj@gmail.com>
2022-08-15 17:36:56 -0400

    feat: added template for layout changes

--- src/components/VehicleList.vue
+++ src/components/VehicleList.vue
@@ -7,10 +7,20 @@
             <div class="max-w-7xl mx-auto pb-12 px-4 sm:px-6 lg:px-8">
                 <!-- Heading Row -->
                 <div class="pb-5 sm:flex sm:items-center sm:justify-between">
-                    <select @change="filterChange" v-model="filter" class="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                        <option value="all">All Vehicles</option>
-                        <option value="my" selected>My Vehicles</option>
-                    </select>
+                    <div class="flex items-center gap-4">
+                        <select @change="filterChange" v-model="filter" class="mt-1 block pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                            <option value="all">All Vehicles</option>
+                            <option value="my" selected>My Vehicles</option>
+                        </select>
+                        <button type="button" @click.prevent="toggleLayout">
+                            <svg v-if="!layoutGrid" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#FFFC79" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                            </svg>
+                            <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="#FFFC79" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+                            </svg>
+                        </button>
+                    </div>
                     <div>
                         <button type="button" @click.prevent="createVehicle()" class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-aftrBlue bg-white hover:bg-aftrBlue hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-aftrBlue">
                             <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -99,6 +109,7 @@ export default {
             showResult: true,
             hasNextPage: false,
             showCreateSimple: false,
+            layoutGrid: true,
         };
     },
     // mounted() {
@@ -108,6 +119,9 @@ export default {
         ...mapGetters(["getAftrContractSrcId", "getEvolvedContractSrcId"]),
     },
     methods: {
+        toggleLayout() {
+            this.layoutGrid = !this.layoutGrid;
+        },
         createVehicle() {
             this.$router.push("create");
         },
