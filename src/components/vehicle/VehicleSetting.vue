32b19175e6490f4406ba9538e8cae3a326ddfb72
arcj0 <baumerj@gmail.com>
2022-08-23 17:57:31 -0400

    chore: UI shadow

--- src/components/vehicle/VehicleSetting.vue
+++ src/components/vehicle/VehicleSetting.vue
@@ -14,8 +14,8 @@
     <perfect-scrollbar>
     <div :class="editClass">
         <!-- Setting Table -->
-        <div class="col-span-3 pt-2 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-            <table class="min-w-full divide-y divide-gray-200">
+        <div class="shadow col-span-3 overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table class="w-full divide-y divide-gray-200">
                 <thead class="bg-gray-50">
                     <tr>
                         <th scope="col" class="flex space-x-3 px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
