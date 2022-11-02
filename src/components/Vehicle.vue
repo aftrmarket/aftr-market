6812dd38a6c0d264c6ab3c63103a3598151185ff
Prajakta More <prajakta.more@harbingergroup.com>
2022-11-02 10:19:08 +0530

    popup for copy button

--- src/components/Vehicle.vue
+++ src/components/Vehicle.vue
@@ -19,12 +19,46 @@
                             <h1 class="text-2xl font-bold text-white">{{ vehicle.name }}</h1>
                             <p class="text-sm font-medium font-mono text-aftrYellow">
                                 {{ contractId }}
-                                <button v-clipboard="contractId">
+                                <button v-clipboard="contractId" @click="showPopup">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                                     </svg>
                                 </button>
+
+                                
+                                <!-- </div>
+                            </div> -->
+
+
                             </p>
+                        </div>
+                    </div>
+                    <div aria-live="assertive" class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+                        <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
+                        <transition enter-active-class="transform ease-out duration-300 transition" enter-from-class="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2" enter-to-class="translate-y-0 opacity-100 sm:translate-x-0" leave-active-class="transition ease-in duration-100" leave-from-class="opacity-100" leave-to-class="opacity-0">
+                            <div v-if="show" class="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                                <div class="p-4">
+                                    <div class="flex items-start">
+                                    <div class="flex-shrink-0">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 bg-green-100">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                            </svg>
+                                    </div>
+                                    <div class="ml-3 w-0 flex-1 pt-0.5">
+                                        <p class="text-sm font-medium text-gray-900">Copied address to clipboard</p>
+                                    </div>
+                                    <div class="ml-4 flex flex-shrink-0">
+                                        <button type="button" @click="show = false" class="inline-flex rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                                            <span class="sr-only">Close</span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                            </svg>
+                                        </button>
+                                    </div>
+                                    </div>
+                                </div>
+                             </div>
+                        </transition>
                         </div>
                     </div>
                     <div class="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
@@ -143,6 +177,7 @@ export default {
             showEvolveModal: false,
             evolvePressedAlready: false,
             allowEdits: false,
+            show: false
         };
     },
     computed: {
@@ -163,6 +198,9 @@ export default {
         ...mapGetters(["getActiveAddress", "getAftrContractSrcId", "getEvolvedContractSrcId", "currentBlock"]),
     },
     methods: {
+        showPopup(){
+            this.show = true;
+        },
         tabText(tab) {
             let cssText = "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm ";
             if (tab.current) {
