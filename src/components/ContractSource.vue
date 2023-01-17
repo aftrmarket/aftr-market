86f37d07d6ec45aaff046bee12ff999d7bb7b82c
Dan Barzyk <danbarzyk@mbp.local>
2023-01-17 17:47:54 -0500

    fix: ContractSource using prop to fetch contracts. Error message added for when contractId has no contracts created with it

--- src/components/ContractSource.vue
+++ src/components/ContractSource.vue
@@ -10,21 +10,22 @@
                 </div>
                 <div>
                     <h1 class="text-2xl font-bold text-white">AFTR Contract Source</h1>
-                    <p class="text-sm font-medium font-mono text-aftrYellow">
-                        {{ this.contractId }}
+                    <div class="flex items-center space-x-2 text-sm font-medium font-mono text-aftrYellow">
+                        <span>{{ this.contractId }}</span>
                         <button v-clipboard="this.contractId" @click="showPopup">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
                                     d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
                             </svg>
                         </button>
-                    </p>
+                    </div>
                 </div>
             </div>
         </div>
         <div class="py-6 bg-gray-50 overflow-hidden">
             <div class="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8 text-sm">
-                <highlightjs language="js" :code="contractSrc" />
+                <highlightjs v-if="contractSrc != '' && contractSrc != undefined" language="js" :code="contractSrc" />
+                <p v-if="contractSrc == undefined" class="text-red-500">Contract source missing - no contracts exist with source ID {{ contractId }}</p>
             </div>
         </div>
     </main>
@@ -132,18 +133,23 @@ export default {
         if (import.meta.env.VITE_ENV === "PROD" || import.meta.env.VITE_ENV === "TEST") {
             this.$store.commit("setAftrContractSources");
             const csArray = this.getAftrContractSources
-            let aftrContractSourceId = csArray[csArray.length - 1]
+            let aftrContractSourceId = this.contractId
             // get a contract that uses this source
             let route = 'https://gateway.redstone.finance/gateway/contracts-by-source?id=' + aftrContractSourceId + (this.network === 'TEST' ? '&testnet=true' : '');
             let response = await fetch(route)
             let data = await response.json()
-            let contract = data.contracts[0]
-            let contractId = contract.contractId
 
-            route = 'https://gateway.redstone.finance/gateway/contract?txId=' + contractId + (this.network === 'TEST' ? '&testnet=true' : '');
-            response = await fetch(route)
-            data = await response.json()
-            this.contractSrc = data.src
+            let contract = data.contracts[0]
+            if (contract != undefined) {
+                let contractId = contract.contractId
+
+                route = 'https://gateway.redstone.finance/gateway/contract?txId=' + contractId + (this.network === 'TEST' ? '&testnet=true' : '');
+                response = await fetch(route)
+                data = await response.json()
+                this.contractSrc = data.src
+            } else {
+                this.contractSrc = undefined
+            }
         } else {
             // Check to see if any contract sources exist (getAftrContractSources variable)
             const csArray = this.getAftrContractSources
