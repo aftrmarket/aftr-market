fbe1797bf2da6bfedcd3408d571d2fe0a19674dc
Dan Barzyk <danbarzyk@mbp.local>
2023-01-18 17:17:06 -0500

    RepoList/RepoTable changed loading animation

--- src/components/RepoTable.vue
+++ src/components/RepoTable.vue
@@ -1,9 +1,9 @@
 <template>
   <!-- <main class="-mt-32"> -->
-  <div class="px-4 sm:px-6 lg:px-8">
-    <div class="mt-8 flex flex-col">
-      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+  <div class="px-8">
+    <div class="flex flex-col">
+      <div class="overflow-x-auto">
+        <div class="inline-block min-w-full align-middle">
           <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
             <table class="min-w-full divide-y divide-gray-300">
               <thead class="bg-gray-50">
