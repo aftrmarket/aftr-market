538f6f62c130392b763661f6af308c5c7d8727a1
arcj0 <baumerj@gmail.com>
2023-03-20 11:29:01 -0400

    fix: privlege change condition

--- src/components/repo/RepoInfo.vue
+++ src/components/repo/RepoInfo.vue
@@ -129,9 +129,9 @@
                     <p>The following functions are optional. By turning these off you will be limiting your repo's functionality, but you may have good reason to do so.</p>
                     <ul class="list-disc pl-6">
                         <li class="py-2"><b>Transfer</b> - Gives the repo the ability to transfer membership balances.</li>
-                        <li class="pb-2"><b>Deposit</b> - Allows anyone to deposit supported Arweave assets into the repo.</li>
-                        <li class="pb-2"><b>Allow</b> - Required for tradability protocols such as Verto Flex.</li>
-                        <li class="pb-2"><b>Claim</b> - Required for tradability protocols such as Verto Flex.</li>
+                        <li class="pb-2"><b>Deposit</b> - Allows anyone to deposit supported Arweave assets into this repo.</li>
+                        <li class="pb-2"><b>Allow</b> - Required for tradability protocols such as Verto Flex and for depositing this repo into another AFTR Repo.</li>
+                        <li class="pb-2"><b>Claim</b> - Required for tradability protocols such as Verto Flex and for depositing this repo into another AFTR Repo.</li>
                         <li><b>Multi-Interactions</b> - Gives the repo the ability to perform more than one change at a time.</li>
                     </ul>
                 </div>
@@ -403,14 +403,16 @@ export default {
             }
 
             // Determine if functions have changed
-            const equal = this.selectedFunctions.length === this.repo.functions.length && this.selectedFunctions.every((value, index) => value === this.repo.functions[index]);
+            const equal = this.selectedFunctions.length === this.repo.functions.length && this.selectedFunctions.every(item => this.repo.functions.includes(item));
 
             if (equal) {
-                this.isFormValid = false;
                 this.funcChange = false;
             } else {
-                this.isFormValid = true;
                 this.funcChange = true;
+            }
+
+            if (this.isFormValid && this.funcChange) {
+                this.isFormValid = true;
             }
         },
         async updateRepo() {
@@ -543,6 +545,12 @@ export default {
                     }
                 } else {
                     // Invalid
+                    this.$swal({
+                        icon: "info",
+                        html: "No changes have been made.",
+                        showConfirmButton: true,
+                        allowOutsideClick: false,
+                    });
                     return;
                 }
 
